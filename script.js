9c5b95a5c1fed6720d5e0a566375db9b45433741
Anay Sharma <anaysharma8791@gmail.com>
2022-07-11 09:50:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function add (num1, num2) {	return num1 + num2;}
+function subtract (num1, num2) { return num1 - num2;}
+function multiply (num1, num2) { return num1 * num2;}
+function divide (num1, num2) { return num1 / num2;}
+
+function operate (operater, num1, num2) {
+	
+}
